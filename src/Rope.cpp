c39df9b5eb2fb983e68ec91b27e2e5d382ae9969
Husnain Haider <16mhh6@queensu.ca>
2019-11-24 18:35:49 -0500

    cleaned up rope class

--- src/Rope.cpp
+++ src/Rope.cpp
@@ -29,7 +29,6 @@ Rope::Rope(Player *attached, Entity* swingingBlock) {
 
 void Rope::update() {
 	time = (SDL_GetTicks() - ropeStart)*(PI/180);
-	//attached->setX(int(length*-sin(angularFreq*time+initAngle))+swingingBlock->getX());
 	attached->setYVelocity(int(length*angularFreq*-sin(angularFreq*time+initAngle)));
 	attached->setY(int(length*cos(angularFreq*time+initAngle))+swingingBlock->getY());
 	attached->setXVelocity(int(length*angularFreq*-cos(angularFreq*time+initAngle)/2.8));//2.8 is just a constant that slows down the screen since it is too fast without it
@@ -46,38 +45,3 @@ void Rope::draw(Graphics& graphics) {
 	graphics.drawLine(attached->getX() + attached->getWidth()/2, attached->getY() + attached->getHeight()/2, swingingBlock->getX()+swingingBlock->getWidth()/2, swingingBlock->getY() + swingingBlock->getHeight());
 	SDL_SetRenderDrawColor(graphics.getRenderer(), 0, 0, 0, 0);
 }
-
-//
-//void Rope::initiliaze(int x, int y) {
-//
-//	for(int i = 0; i < rope_constants::NODES; i++) {
-//		RopeNode node;
-//		node.positionX = x + i * 2;
-//		node.positionY = y;
-//		node.oldPositionX = node.positionX;
-//		node.oldPositionY = node.positionY;
-//		node.velocityX = 0;
-//		node.velocityY = 0;
-//		node.mass = i;
-//
-//		this->segments.push_back(node);
-//	}
-//}
-//
-//void Rope::draw(Graphics& graphics) {
-//	for(size_t i = 0; i < segments.size()-1; i++) {
-//		RopeNode nodeOne = segments.at(i);
-//		RopeNode nodeTwo = segments.at(i+1);
-//
-//		int x1 = nodeOne.positionX;
-//		int y1 = nodeOne.positionY;
-//		int x2 = nodeTwo.positionX;
-//		int y2 = nodeTwo.positionY;
-//
-//		cout << "ma" << x1 << endl;
-//		SDL_SetRenderDrawColor(graphics.getRenderer(), 255, 255, 255, 255);
-//
-//		graphics.drawLine(x1, y1, x2, y2);
-//		SDL_SetRenderDrawColor(graphics.getRenderer(), 0, 0, 0, 0);
-//
-//	}
