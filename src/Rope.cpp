9fbcaf3b9ad8698709fa65a9c77ce2feec1d3c34
Daniyal <mdaniyalmaniar@hotmail.com>
2019-11-25 14:34:37 -0500

    Commented Rope class

--- src/Rope.cpp
+++ src/Rope.cpp
@@ -10,36 +10,39 @@ namespace rope_constants {
 	int NODES = 10;
 }
 
+// Constant for PI
 const double PI = 3.14159265;
 
 
 Rope::Rope(Player *attached, Entity* swingingBlock) {
-	this->attached = attached;
-	this->swingingBlock = swingingBlock;
-	this->attached->getGameWin()->getSoundMixer().playSound(attached->getGameWin(), "rope");
-	this->vectorX = swingingBlock->getX()-attached->getX();
-	this->vectorY = -swingingBlock->getY()+attached->getY();
-	this->time = 0;
-	this->length = sqrt((vectorX*vectorX)+(vectorY*vectorY));
-	this->initAngle = acos(abs(vectorY)/length);
-	this->ropeStart = SDL_GetTicks();
-	this->angularFreq = -0.1*(350/length);
+	// Initializes the rope class with the required variables
+	this->attached = attached; // Player that is attached
+	this->swingingBlock = swingingBlock; // Block that the player is attached to
+	this->attached->getGameWin()->getSoundMixer().playSound(attached->getGameWin(), "rope"); // Play the sound for the rope
+	this->vectorX = swingingBlock->getX()-attached->getX(); // Initializing the y vector of the rope
+	this->vectorY = -swingingBlock->getY()+attached->getY(); // Initializing the x vector of the rope
+	this->time = 0; // time init
+	this->length = sqrt((vectorX*vectorX)+(vectorY*vectorY)); // getting the length of the rope
+	this->initAngle = acos(abs(vectorY)/length); // calculating the initial angle
+	this->ropeStart = SDL_GetTicks(); // Getting the start time of the rope
+	this->angularFreq = -0.1*(350/length); // Calculating the angular velocity based upon the vectors
 
 }
 
 void Rope::update() {
-	time = (SDL_GetTicks() - ropeStart)*(PI/180);
-	attached->setYVelocity(int(length*angularFreq*-sin(angularFreq*time+initAngle)));
-	attached->setY(int(length*cos(angularFreq*time+initAngle))+swingingBlock->getY());
-	attached->setXVelocity(int(length*angularFreq*-cos(angularFreq*time+initAngle)/2.8));//2.8 is just a constant that slows down the screen since it is too fast without it
+	time = (SDL_GetTicks() - ropeStart)*(PI/180); // calculate current time since init
+	attached->setYVelocity(int(length*angularFreq*-sin(angularFreq*time+initAngle))); // Calc the Y velocity based upon time
+	attached->setY(int(length*cos(angularFreq*time+initAngle))+swingingBlock->getY()); // Render Y displacement based upon time
+	attached->setXVelocity(int(length*angularFreq*-cos(angularFreq*time+initAngle)/2.8)); // Calc the Y velocity based upon time for scrolling mechanic
 
 
 }
-
+// accessor for the attached block
 Entity* Rope::getSwingingBlock() const {
 	return swingingBlock;
 }
 
+// Code to render a line in sdl between the player and the block
 void Rope::draw(Graphics& graphics) {
 	SDL_SetRenderDrawColor(graphics.getRenderer(), 255, 255, 255, 255);
 	graphics.drawLine(attached->getX() + attached->getWidth()/2, attached->getY() + attached->getHeight()/2, swingingBlock->getX()+swingingBlock->getWidth()/2, swingingBlock->getY() + swingingBlock->getHeight());
